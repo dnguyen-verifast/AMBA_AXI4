0eb02741b4e76783f78cbf03130f876d3869944f
dnguyen-verifast <dnguyen@verifasttech.com>
2026-05-18 11:39:58 +0700

    sssss

--- src/hdl_top/slave_agent_bfm/axi4_slave_monitor_bfm.sv
+++ src/hdl_top/slave_agent_bfm/axi4_slave_monitor_bfm.sv
@@ -130,7 +130,7 @@ interface axi4_slave_monitor_bfm(input aclk, input aresetn,
   //-------------------------------------------------------
   task axi4_slave_write_data_sampling(output axi4_write_transfer_char_s req ,input axi4_transfer_cfg_s cfg, output int beat_count);
     static reg[7:0] i = 0;
-    
+    beat_count = 0;
   forever begin
    // wait for valid and ready to be high
    do begin
